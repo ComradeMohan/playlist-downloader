ccc2d882d934310cd08965e09c128e0e4d9d4770
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2024-10-17 22:58:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, FileResponse
 import yt_dlp
-import time
+import os
 import logging
 import zipfile
-import os
 
 app = FastAPI()
 
@@ -43,7 +42,6 @@ def hook(d):
 @app.post("/download/")
 async def download(playlist_url: str = Form(...)):
     try:
-        # Create a directory for the playlist if it doesn't exist
         playlist_name = playlist_url.split("list=")[-1]
         os.makedirs(playlist_name, exist_ok=True)
 
@@ -55,17 +53,17 @@ async def download(playlist_url: str = Form(...)):
         with zipfile.ZipFile(zip_filename, 'w') as zipf:
             for root, dirs, files in os.walk(playlist_name):
                 for file in files:
-                    zipf.write(os.path.join(root, file), file)
+                    zipf.write(os.path.join(root, file), arcname=file)
 
-        time.sleep(1)  # Sleep to reduce detection
-        return {"status": "success", "message": "Playlist download started", "zip_file": zip_filename}
+        return {"status": "success", "message": "Playlist downloaded successfully", "zip_file": zip_filename}
     except Exception as e:
         logging.error(f"Endpoint error: {str(e)}")
         return {"status": "error", "message": str(e)}
 
 @app.get("/download-zip/{zip_filename}")
 async def download_zip(zip_filename: str):
-    return FileResponse(zip_filename)
+    # Serve the ZIP file for download
+    return FileResponse(zip_filename, media_type='application/zip', filename=zip_filename)
 
 @app.get("/", response_class=HTMLResponse)
 async def serve_html():
